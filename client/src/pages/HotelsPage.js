6560f89174c397415bc09cc8f6ee49c8e99982b6
Marc Vaz <marcvaz2020@gmail.com>
2024-05-05 00:27:53 -0400

    fix formatting

--- client/src/pages/HotelsPage.js
+++ client/src/pages/HotelsPage.js
@@ -42,32 +42,32 @@ export default function HotelsPage() {
   const format4 = { display: 'grid', justifyContent: 'space-evenly' };
 
   return (
-  <>
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px' }}>
-      <h2 style={{ width: '80%', margin: '0 auto', marginTop: '30px', marginBottom: '15px', fontSize: '35px', textAlign: 'center' }}>Top hotels in popular cities:</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-        {hotels.map((hotel) =>
-          <div
-            key={hotel.hotel_id}
-            style={{
-              width: '45%',
-              padding: '10px',
-              margin: '10px 2.5%',
-              border: '1px solid #000',
-              borderRadius: '5px',
-              background: '#c3d4e2'
-            }}
-          >
-          <NavLink to={`/hotels/${hotel.hotel_id}`}>
-            <h4>{hotel.city_name} - {hotel.hotel_name}</h4>
-          </NavLink>
-          <p>{hotel.address.split(/(?=[A-Z])/).join(' ')}</p>
-          <p>Rating: {hotel.rating} / 5</p>
+    <>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px' }}>
+        <h2 style={{ width: '80%', margin: '0 auto', marginTop: '30px', marginBottom: '15px', fontSize: '35px', textAlign: 'center' }}>Top hotels in popular cities:</h2>
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+          {hotels.map((hotel) => (
+            <div
+              key={hotel.hotel_id}
+              style={{
+                width: '45%',
+                padding: '10px',
+                margin: '10px 2.5%',
+                border: '1px solid #000',
+                borderRadius: '5px',
+                background: '#c3d4e2'
+              }}
+            >
+              <NavLink to={`/hotels/${hotel.hotel_id}`}>
+                <h4>{hotel.city_name} - {hotel.hotel_name}</h4>
+              </NavLink>
+              <p>{hotel.address.split(/(?=[A-Z])/).join(' ')}</p>
+              <p>Rating: {hotel.rating} / 5</p>
+            </div>
+          ))}
         </div>
-      )}
-    </div>
-    </div>
-    <h2 style={{ fontWeight: 'normal', fontSize: '1em', color: 'gray' }}>Developed by Eshaan Chichula, Shruthi Kunjur, Marc Vaz </h2>
-  </>
-);
+      </div>
+      <h2 style={{ fontWeight: 'normal', fontSize: '1em', color: 'gray' }}>Developed by Eshaan Chichula, Shruthi Kunjur, Marc Vaz </h2>
+    </>
+  );
 }
